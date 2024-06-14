635b01e3ef14195dd0abbafd990f6d0731c647a8
gabi!!! <108476432+GabiRomeroh@users.noreply.github.com>
2024-06-14 11:28:21 -0300

    aaaa

---
+++ script.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoPerguntas = document.querySelector(".caixa-resultado");
+
+const= "pergunta 1",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2",
+]
+{
+}
+enunciado: "Pergunta 2"
+lternativas: [
+"Alternativa 1",
+"Alternativa 2",
+]
