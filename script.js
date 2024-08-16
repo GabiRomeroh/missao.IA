2e305dae8862ce53e727c1f5811a636d751e5328
gabi!!! <108476432+GabiRomeroh@users.noreply.github.com>
2024-08-16 12:10:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
+
+// Seleciona os elementos HTML que manipulados
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-principal");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoPerguntas = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const= "pergunta 1",
-alternativas: [
-    "Alternativa 1",
-    "Alternativa 2",
-]
-[ 
-
-
-{
-
-    enunciado: "Pergunta 2"
-    lternativas: 
-    "Alternativa 1",
-    "Alternativa 2",
+//Arry de objeto contendo as perguntas e alternativas
+const perguntas = [
+    {
+        enunciado: "Qual é o maior deserto do mundo?",
+        alternativas: [
+            "Antártica",
+            "Saara"
+        ],
+        correta: 2// A segunda alternativa é a correta
+    },
+    {
+        enunciado: "Qual a fórmula da glicose?",
+        alternativas: [
+            "C6H12O6",
+            "C10H16N5013P3"
+        ],
+        correta: 1 // A primeria alternativa é a correta
+    },
+    {
+        enunciado: "Qual empresa desenvolveu o sistema Windows?",
+        alternativas: [
+            "Apple",
+            "Microsoft"
+        ],
+        correta: 1
+    },
+    {
+        enunciado: "Quem é o capitão do navio Pérola Negra?",
+        alternativas: [
+            "Jack Skellington",
+            "Jack Sparrow"
+        ],
+        correta: 2
+    },
+    {
+        enunciado: "Qual o maior oceno do planeta Terra?",
+        alternativas: [
+            "Oceano Pacífico",
+            "Oceano Atlântico"
+        ],
+        correta: 1
     }
-]
+];
+
+// inicializa o índice da pergunta atual e a pontuação
+
+let atual = 0;
+let perguntaAtual;
+let pontuacao = 0; // Inicie a pontuação em 0
