32c78e0f7385de2ec8762d32e37b2f659f4a2ca5
gabi!!! <108476432+GabiRomeroh@users.noreply.github.com>
2024-10-11 10:45:55 -0300

    jj

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-// Seleciona os elementos HTML que serão manipulados
+
+// Seleciona os elementos HTML que manipulados
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-// Array de objetos contendo as perguntas e alternativas
+//Arry de objeto contendo as perguntas e alternativas
 const perguntas = [
     {
         enunciado: "Qual a idade máxima que um cachorro pode chegar?",
         alternativas: [
             "15 anos",
             "20 anos"
-   imagem: "url_da_imagem_do_cachorro.jpg" // Adicione a URL da image
-            
-            ];
-            
-
-
         ],
         correta: 0 // A primeira alternativa é a correta
-
+    },
     {
         enunciado: "Quantos dias há em um ano bissexto?",
         alternativas: [
@@ -55,24 +50,33 @@ const perguntas = [
     }
 ];
 
-let atual = 0;
-let perguntaAtual; // Corrigido de "pergutaAtual" para "perguntaAtual"
+let atual = 0
+let pergutaAtual;
 let pontuacao = 0;
 
-// FUNÇÃO MOSTRAR PERGUNTAS
+//FUNÇÃO MOSTRAR PERGUNTAS
 function mostrarPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.innerHTML = "";
 
-    perguntaAtual.alternativas.forEach((alternativa, index) => { // Corrigido "pergutaAtual" para "perguntaAtual"
+    pergutaAtual.alternativas.forEach((alternativas, index) => {
         const botao = document.createElement("button");
-        botao.textContent = alternativa; // Adiciona o texto da alternativa ao botão
         botao.addEventListener("click", () => verificaResposta(index));
         caixaAlternativas.appendChild(botao);
     });
 }
 
 // FUNÇÃO VERIFICAR RESPOSTA
-function verificaResposta(selecionada) { // Corrigido de "Seleciona" para "selecionada"
-    if (selecionada === perguntaAt
+function vereficaResposta(Seleciona) {
+    if (selecionda === pergutaAtual.correta) {
+        pontuacao++;
+    }
+    atual++;
+
+    if (atual < perguntas.length) {
+        mostrarPergunta();
+    } else {
+        mostarResultado();
+    }
+}
